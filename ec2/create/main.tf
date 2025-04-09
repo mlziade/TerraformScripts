2fad6e5ab2254dc39e72fa4a1999abffb36d9159
mlziade <ziade.michel@hotmail.com>
2025-04-08 22:57:31 -0300

    Add security group configuration and output for public IP in ec2/create

--- ec2/create/main.tf
+++ ec2/create/main.tf
@@ -2,9 +2,50 @@ provider "aws" {
   region = "<<REGION>>"
 }
 
+resource "aws_security_group" "<<SECURITY_GROUP_NAME>>" {
+  name        = "<<SECURITY_GROUP_NAME>>"
+  description = "Allow SSH and HTTP"
+  
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "TLS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "EC2 SG"
+  }
+}
+
 resource "aws_instance" "<<RESOURCE_NAME>>" {
   ami           = var.ami_id
   instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.<<SECURITY_GROUP_NAME>>.id]
 
   tags = {
     Name = "<<TAG_NAME>>"
--- ec2/create/outputs.tf
+++ ec2/create/outputs.tf
@@ -2,3 +2,8 @@ output "instance_id" {
   description = "ID of the EC2 instance"
   value       = aws_instance.<<RESOURCE_NAME>>.id
 }
+
+output "public_ip" {
+  description = "Public IP address of the instance"
+  value       = aws_instance..<<RESOURCE_NAME>>.public_ip
+}
